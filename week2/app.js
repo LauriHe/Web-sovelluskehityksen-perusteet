eb855632a8f07aac3d071aad7b40fa60f558675c
LauriHe <Lauri.Heikkinen5@metropolia.fi>
2022-11-01 10:46:00 +0200

    week2 catRoutes

--- week2/app.js
+++ week2/app.js
@@ -1,27 +1,9 @@
 'use strict';
 const express = require('express');
+const catRoute = require('./routes/catRoute');
 const app = express();
 const port = 3000;
 
-app.get('/cat', (req, res) => {
-  res.send('From this endpoint you can get cats.');
-});
-
-app.post('/cat', (req, res) => {
-  res.send('From this endpoint you can add cats.');
-});
-
-app.put('/cat', (req, res) => {
-  res.send('From this endpoint you can edit cats.');
-});
-
-app.delete('/cat', (req, res) => {
-  res.send('From this endpoint you can remove cats.');
-});
-
-app.get('/cat/:id', (req, res) => {
-  console.log('Get cat :id', req.params);
-  res.send('You requested a cat with id: ' + req.params.id);
-});
+app.use('/cat', catRoute);
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
--- week2/routes/catRoute.js
+++ week2/routes/catRoute.js
@@ -1,2 +1,28 @@
 'use strict';
 // catRoute
+
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  res.send('From this endpoint you can get cats.');
+});
+
+router.get('/:id', (req, res) => {
+  console.log('Get cat :id', req.params);
+  res.send('You requested a cat with id: ' + req.params.id);
+});
+
+router.post('/', (req, res) => {
+  res.send('From this endpoint you can add cats.');
+});
+
+router.put('/', (req, res) => {
+  res.send('From this endpoint you can edit cats.');
+});
+
+router.delete('/', (req, res) => {
+  res.send('From this endpoint you can remove cats.');
+});
+
+module.exports = router;
